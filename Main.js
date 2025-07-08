8d8c80f8ecd39958b151b8efff2796720cc3af6a
Watkins <32805045+Ninjawarrior81@users.noreply.github.com>
2025-07-08 09:54:13 +0900

    Got JS file to Work!
    Added the English version of the introduction flyer.
    Edited the JS file to change between English and Japanese Flyer.

--- Main.js
+++ Main.js
@@ -1,1 +1,8 @@
 // JavaScript source code
+
+function allFlyerChangeENG() {
+    document.getElementById('CEJI').src = 'Media/Images/CEE.png';
+}
+function allFlyerChangeJAP() {
+    document.getElementById('CEJI').src = 'Media/Images/CEJ.png';
+}
