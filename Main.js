a181bd6ca4676f3634a0b4956900b79e297296ab
Watkins <32805045+Ninjawarrior81@users.noreply.github.com>
2025-07-08 14:25:41 +0900

    Weekend Menu and Thursday Menu
    Added the English version of those docs
    Simplified file names

--- Main.js
+++ Main.js
@@ -2,7 +2,11 @@
 
 function allFlyerChangeENG() {
     document.getElementById('CEJI').src = 'Media/Images/CEE.png';
+    document.getElementById('TMEJ').src = 'Media/Images/TMEE.png';
+    document.getElementById('WMEJ').src = 'Media/Images/WMEE.png';
 }
 function allFlyerChangeJAP() {
     document.getElementById('CEJI').src = 'Media/Images/CEJ.png';
+    document.getElementById('TMEJ').src = 'Media/Images/TMEJ.png';
+    document.getElementById('WMEJ').src = 'Media/Images/WMEJ.png';
 }
