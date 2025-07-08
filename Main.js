48f90bad3dad3614bf06fc9b0d550c1d02ab30da
Watkins <32805045+Ninjawarrior81@users.noreply.github.com>
2025-07-08 17:18:21 +0900

    Uniformity and Privacy
    Removed more files for privacy
    Also edited references for uniformity

--- Main.js
+++ Main.js
@@ -5,10 +5,18 @@ function allFlyerChangeENG() {
     document.getElementById('TME').src = 'Media/Images/TMEE.png';
     document.getElementById('WME').src = 'Media/Images/WMEE.png';
     document.getElementById('PT').src = 'Media/Images/PTE.png';
+    document.getElementById('CEL').setAttribute('href', 'Media/Images/CEE.png');
+    document.getElementById('TME').setAttribute('href', 'Media/Images/TMEE.png');
+    document.getElementById('WME').setAttribute('href', 'Media/Images/WMEE.png');
+    document.getElementById('PT').setAttribute('href', 'Media/Images/PTE.png');
 }
 function allFlyerChangeJAP() {
     document.getElementById('CEI').src = 'Media/Images/CEJ.png';
     document.getElementById('TME').src = 'Media/Images/TMEJ.png';
     document.getElementById('WME').src = 'Media/Images/WMEJ.png';
     document.getElementById('PT').src = 'Media/Images/PTJ.png';
+    document.getElementById('CEL').setAttribute('href', 'Media/Images/CEJ.png');
+    document.getElementById('TME').setAttribute('href', 'Media/Images/TMEJ.png');
+    document.getElementById('WME').setAttribute('href', 'Media/Images/WMEJ.png');
+    document.getElementById('PT').setAttribute('href', 'Media/Images/PTJ.png');
 }
