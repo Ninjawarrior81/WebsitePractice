f684f8a2ba2f28f5a855669f7428548306bca9ee
Watkins <32805045+Ninjawarrior81@users.noreply.github.com>
2025-07-08 18:13:11 +0900

    More organization
    Organized File Names
    Prepared JS for English versions
    Put all cafe-related images in the CafeExplanation folder.

--- Main.js
+++ Main.js
@@ -1,22 +1,36 @@
 // JavaScript source code
 
 function allFlyerChangeENG() {
-    document.getElementById('CEI').src = 'Media/Images/CEE.png';
-    document.getElementById('TME').src = 'Media/Images/TMEE.png';
-    document.getElementById('WME').src = 'Media/Images/WMEE.png';
-    document.getElementById('PT').src = 'Media/Images/PTE.png';
-    document.getElementById('CEL').setAttribute('href', 'Media/Images/CEE.png');
-    document.getElementById('TME').setAttribute('href', 'Media/Images/TMEE.png');
-    document.getElementById('WME').setAttribute('href', 'Media/Images/WMEE.png');
-    document.getElementById('PT').setAttribute('href', 'Media/Images/PTE.png');
+    document.getElementById('CSEI').src = 'Media/Images/CafeExplanation/CSEE.png';
+    document.getElementById('WMEI').src = 'Media/Images/CafeExplanation/WMEE.png';
+    document.getElementById('TMEI').src = 'Media/Images/CafeExplanation/TMEE.png';
+    document.getElementById('PTEI').src = 'Media/Images/CafeExplanation/PTEE.png';
+    document.getElementById('VDEI').src = 'Media/Images/CafeExplanation/VDEE.png';
+    document.getElementById('ICEI').src = 'Media/Images/CafeExplanation/ICEE.png';
+    document.getElementById('CMEI').src = 'Media/Images/CafeExplanation/CMEE.png';
+
+    document.getElementById('CSEL').setAttribute('href', 'Media/Images/CafeExplanation/CSEE.png');
+    document.getElementById('WMEL').setAttribute('href', 'Media/Images/CafeExplanation/WMEE.png');
+    document.getElementById('TMEL').setAttribute('href', 'Media/Images/CafeExplanation/TMEE.png');
+    document.getElementById('PTEL').setAttribute('href', 'Media/Images/CafeExplanation/PTEE.png');
+    document.getElementById('VDEL').setAttribute('href', 'Media/Images/CafeExplanation/VDEE.png');
+    document.getElementById('ICEL').setAttribute('href', 'Media/Images/CafeExplanation/ICEE.png');
+    document.getElementById('CMEL').setAttribute('href', 'Media/Images/CafeExplanation/CMEE.png');
 }
 function allFlyerChangeJAP() {
-    document.getElementById('CEI').src = 'Media/Images/CEJ.png';
-    document.getElementById('TME').src = 'Media/Images/TMEJ.png';
-    document.getElementById('WME').src = 'Media/Images/WMEJ.png';
-    document.getElementById('PT').src = 'Media/Images/PTJ.png';
-    document.getElementById('CEL').setAttribute('href', 'Media/Images/CEJ.png');
-    document.getElementById('TME').setAttribute('href', 'Media/Images/TMEJ.png');
-    document.getElementById('WME').setAttribute('href', 'Media/Images/WMEJ.png');
-    document.getElementById('PT').setAttribute('href', 'Media/Images/PTJ.png');
+    document.getElementById('CSEI').src = 'Media/Images/CafeExplanation/CSEJ.png';
+    document.getElementById('WMEI').src = 'Media/Images/CafeExplanation/WMEJ.png';
+    document.getElementById('TMEI').src = 'Media/Images/CafeExplanation/TMEJ.png';
+    document.getElementById('PTEI').src = 'Media/Images/CafeExplanation/PTEJ.png';
+    document.getElementById('VDEI').src = 'Media/Images/CafeExplanation/VDEJ.png';
+    document.getElementById('ICEI').src = 'Media/Images/CafeExplanation/ICEJ.png';
+    document.getElementById('CMEI').src = 'Media/Images/CafeExplanation/CMEJ.png';
+
+    document.getElementById('CSEL').setAttribute('href', 'Media/Images/CafeExplanation/CSEJ.png');
+    document.getElementById('WMEL').setAttribute('href', 'Media/Images/CafeExplanation/WMEJ.png');
+    document.getElementById('TMEL').setAttribute('href', 'Media/Images/CafeExplanation/TMEJ.png');
+    document.getElementById('PTEL').setAttribute('href', 'Media/Images/CafeExplanation/PTEJ.png');
+    document.getElementById('VDEL').setAttribute('href', 'Media/Images/CafeExplanation/VDEJ.png');
+    document.getElementById('ICEL').setAttribute('href', 'Media/Images/CafeExplanation/ICEJ.png');
+    document.getElementById('CMEL').setAttribute('href', 'Media/Images/CafeExplanation/CMEJ.png');
 }
